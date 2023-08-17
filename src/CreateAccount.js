11c843a98aee2178babd98f4aab6c406f59ede81
Ganesh Landge <ganesh.landge@espressif.com>
2023-08-17 15:21:05 +0530

    Updated

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from "react";
-import axios, {isCancel, AxiosError} from 'axios';
+import axios, { isCancel, AxiosError } from 'axios';
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -33,45 +33,82 @@ const defaultTheme = createTheme();
 
 export default function CreateAccount() {
   const [state, setState] = useState({
-  'email': "",
-  'mobilenumber': ""
-});
-
-const handleInputChange = (event) => {
-  const { name, value } = event.target;
-  setState((prevProps) => ({
-    ...prevProps,
-    [name]: value
-  }));
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  axios({
-    method: 'post',
-    url: '/user',
-    data: {
-      'email':state.email,
-      'Mobile Number':state.mobilenumber,
-      'Aadhaar Number': state.aadhaarnumber,
-      'Title': state.title,
-      'First Name': state.firstname,
-      'Middle Name': state.middlename,
-      'Last Name': state.lastname,
-      'Fathers Name': state.fathersname,
-      'Date of Birth': state.dateofbirth,
-      'Age': state.age,
-      'Gender': state.gender,
-      'Permanent Address': state.permanentaddress,
-      'Temporary Address': state.temporaryaddress,
-      'Admin': state.admin,
-      'Occupation Type': state.occupationtype,
-      'Source of Income': state.sourceofincome,
-      'Annual Income': state.annualincome
-    }
+    "adhar_id": '',
+    "age": '',
+    "annual_income": '',
+    "dob": "",
+    "email": "",
+    "father_name": "",
+    "first_name": "",
+    "gender": "",
+    "income_source": "",
+    "is_admin": '',
+    "last_name": "",
+    "middle_name": "",
+    "mobile_num": '',
+    "occupation_type": "",
+    "perm_add_id": "",
+    "temp_add_id": "",
+    "title": "",
+    "account": ""
   });
-  console.log(state);
-};
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevProps) => ({
+      ...prevProps,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = {
+      'email': state.email,
+      'mobile_num': state.mobilenumber,
+      'adhar_id': state.aadhaarnumber,
+      'title': state.title,
+      'first_name': state.firstname,
+      'middle_name': state.middlename,
+      'last_name': state.lastname,
+      'father_name': state.fathersname,
+      'dob': state.dateofbirth,
+      'age': state.age,
+      'gender': state.gender,
+      'perm_add_id': state.permanentaddress,
+      'temp_add_id': state.temporaryaddress,
+      'is_admin': state.admin,
+      'occupation_type': state.occupationtype,
+      'income_source': state.sourceofincome,
+      'annual_income': state.annualincome
+    }
+    console.log(data)
+    axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8082/user',
+      data: {
+        'email': state.email,
+        'mobile_num': state.mobilenumber,
+        'adhar_id': state.aadhaarnumber,
+        'title': state.title,
+        'first_name': state.firstname,
+        'middle_name': state.middlename,
+        'last_name': state.lastname,
+        'father_name': state.fathersname,
+        'dob': state.dateofbirth,
+        'age': state.age,
+        'gender': state.gender,
+        'perm_add_id': state.permanentaddress,
+        'temp_add_id': state.temporaryaddress,
+        'is_admin': state.admin,
+        'occupation_type': state.occupationtype,
+        'income_source': state.sourceofincome,
+        'annual_income': state.annualincome
+      }
+
+    });
+    console.log(state);
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -92,159 +129,159 @@ const handleSubmit = (event) => {
             Login
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <div className="form-control">
-          <label>Email</label>
-          <input
-            type="text"
-            name="email"
-            value={state.email}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Mobile Number</label>
-          <input
-            type="int"
-            name="mobilenumber"
-            value={state.mobilenumber}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Aadhaar Number</label>
-          <input
-            type="int"
-            name="Aadhaar Number"
-            value={state.aadhaarnumber}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Title</label>
-          <input
-            type="text"
-            name="Title"
-            value={state.title}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>First Name</label>
-          <input
-            type="text"
-            name="First Name"
-            value={state.firstname}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Middle Name</label>
-          <input
-            type="text"
-            name="Middle Name"
-            value={state.middlename}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="Last Name"
-            value={state.lastname}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Father's Name</label>
-          <input
-            type="text"
-            name="Father's Name"
-            value={state.fathersname}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Date of Birth</label>
-          <input
-            type="text"
-            name="Date of Birth"
-            value={state.dateofbirth}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Age</label>
-          <input
-            type="int"
-            name="Age"
-            value={state.age}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Gender</label>
-          <input
-            type="text"
-            name="Gender"
-            value={state.gender}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Permanent Address</label>
-          <input
-            type="text"
-            name="Permanent Address"
-            value={state.permanentaddress}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Temporary Address</label>
-          <input
-            type="text"
-            name="Temporary Address"
-            value={state.temporaryaddress}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Admin</label>
-          <input
-            type="text"
-            name="Admin"
-            value={state.admin}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Occupation Type</label>
-          <input
-            type="text"
-            name="Occupation Type"
-            value={state.occupationtype}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Source of Income</label>
-          <input
-            type="text"
-            name="Source of Income"
-            value={state.sourceofincome}
-            onChange={handleInputChange}
-          />
-          </div>
-          <div className="form-control">
-          <label>Annual Income</label>
-          <input
-            type="int"
-            name="Annual Income"
-            value={state.annualincome}
-            onChange={handleInputChange}
-          />
-          </div>
+            <div className="form-control">
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                value={state.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Mobile Number</label>
+              <input
+                type="int"
+                name="mobile_num"
+                value={state.mobilenumber}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Aadhaar Number</label>
+              <input
+                type="int"
+                name="adhar_id"
+                value={state.aadhaarnumber}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Title</label>
+              <input
+                type="text"
+                name="title"
+                value={state.title}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>First Name</label>
+              <input
+                type="text"
+                name="first_name"
+                value={state.firstname}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Middle Name</label>
+              <input
+                type="text"
+                name="middle_name"
+                value={state.middlename}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Last Name</label>
+              <input
+                type="text"
+                name="last_name"
+                value={state.lastname}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Father's Name</label>
+              <input
+                type="text"
+                name="father_name"
+                value={state.fathersname}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Date of Birth</label>
+              <input
+                type="text"
+                name="dob"
+                value={state.dateofbirth}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Age</label>
+              <input
+                type="int"
+                name="age"
+                value={state.age}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Gender</label>
+              <input
+                type="text"
+                name="gender"
+                value={state.gender}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Permanent Address</label>
+              <input
+                type="text"
+                name="perm_add_id"
+                value={state.permanentaddress}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Temporary Address</label>
+              <input
+                type="text"
+                name="temp_add_id"
+                value={state.temporaryaddress}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Admin</label>
+              <input
+                type="text"
+                name="is_admin"
+                value={state.admin}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Occupation Type</label>
+              <input
+                type="text"
+                name="occupation_type"
+                value={state.occupationtype}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Source of Income</label>
+              <input
+                type="text"
+                name="income_source"
+                value={state.sourceofincome}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Annual Income</label>
+              <input
+                type="int"
+                name="income_source"
+                value={state.annualincome}
+                onChange={handleInputChange}
+              />
+            </div>
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
@@ -260,12 +297,12 @@ const handleSubmit = (event) => {
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
-                
+
                 </Link>
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  
+
                 </Link>
               </Grid>
             </Grid>
