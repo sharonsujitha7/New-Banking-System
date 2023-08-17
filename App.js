1e3c9314a3e5c7bbbc6c7fb6c1229cd3da91be8f
Gayathri Magesh <54061255+gayathrimagesh@users.noreply.github.com>
2023-08-17 11:06:08 +0530

    Updated files

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './CreateAccount.css';
+import SignIn from './Login';
+import CreateAccount from './CreateAccount';
+function App() {
+  return (
+    <div className="App">
+      <CreateAccount></CreateAccount>
+    </div>
+  );
+}
+
+export default App;
